0d8c0227d03d99e8faf979627119cb8d90199956
keith <keithw@midland.com.hk>
2019-08-02 15:42:22 +0800

    build

---
+++ build/precache-manifest.57675eac340948dd90a2dee11d2a2780.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b67450ed861b1bbf0abae27b558342",
+    "url": "/dapp/build/index.html"
+  },
+  {
+    "revision": "c96d6b9a12d84d67b732",
+    "url": "/dapp/build/static/js/2.0e5138f8.chunk.js"
+  },
+  {
+    "revision": "808ab45dbce00a60d732",
+    "url": "/dapp/build/static/js/main.e4abe6a5.chunk.js"
+  },
+  {
+    "revision": "0bbb65dac6f0ac33dd3e",
+    "url": "/dapp/build/static/js/runtime~main.27031cc4.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b7bd6970f0799750d2176a736f64099.js"
+  "/dapp/build/precache-manifest.57675eac340948dd90a2dee11d2a2780.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dapp/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
